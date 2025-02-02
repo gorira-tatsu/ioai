f7e9c026d21e8e45efeb55510c4315e8123f5408
gorira <tatsuhiko.shigoto@gmail.com>
2025-02-02 13:29:54 +0900

    First

---
+++ bank-curn/bank_curn/main.py
@@ -0,0 +1,92 @@
+import optuna
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_auc_score
+from xgboost import XGBClassifier
+
+testdf = pd.read_csv("../data/test.csv")
+traindf = pd.read_csv("../data/train.csv")
+
+droped_train = traindf.drop(columns=[
+    "id",
+    "CustomerId",
+    "Surname",
+], axis=1)
+
+droped_train.replace({"Geography": {"France": 0, "Germany": 1, "Spain": 2}}, inplace=True)
+droped_train.replace({"Gender": {"Male": 0, "Female": 1}}, inplace=True)
+
+droped_train["IsZeroBalance"] = droped_train["Balance"].apply(lambda x: 1 if x == 0 else 0)
+droped_train["MiddleAge"] = droped_train["Age"].apply(lambda x: 1 if 45 <= x <= 65 else 0)
+
+droped_test = testdf.drop(columns=[
+    "id",
+    "CustomerId",
+    "Surname",
+], axis=1)
+
+droped_test.replace({"Geography": {"France": 0, "Germany": 1, "Spain": 2}}, inplace=True)
+droped_test.replace({"Gender": {"Male": 0, "Female": 1}}, inplace=True)
+
+droped_test["IsZeroBalance"] = droped_test["Balance"].apply(lambda x: 1 if x == 0 else 0)
+droped_test["MiddleAge"] = droped_test["Age"].apply(lambda x: 1 if 45 <= x <= 65 else 0)
+
+X = droped_train.drop("Exited", axis=1)
+y = droped_train["Exited"]
+
+def objective(trial):
+    learning_rate = trial.suggest_float("learning_rate", 1e-3, 1e-1, log=True)
+    n_estimators = trial.suggest_int("n_estimators", 100, 1000)
+    max_depth = trial.suggest_int("max_depth", 1, 10)
+    subsample = trial.suggest_float("subsample", 0.5, 1.0)
+    colsample_bytree = trial.suggest_float("colsample_bytree", 0.5, 1.0)
+    min_child_weight = trial.suggest_int("min_child_weight", 1, 10)
+    gamma = trial.suggest_float("gamma", 0, 5)
+
+    bst = XGBClassifier(
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        learning_rate=learning_rate,
+        subsample=subsample,
+        colsample_bytree=colsample_bytree,
+        min_child_weight=min_child_weight,
+        gamma=gamma,
+        objective='binary:logistic',
+        use_label_encoder=False,
+        eval_metric="logloss"
+    )
+
+    roc_aucs = []
+    for train_index, test_index in StratifiedKFold.split(X, y):
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        
+        bst.fit(X_train, y_train)
+        preds = bst.predict_proba(X_test)[:, 1]
+        roc_aucs.append(roc_auc_score(y_test, preds))
+
+    return np.mean(roc_aucs)
+
+study = optuna.create_study(direction="maximize")
+study.optimize(objective, n_trials=100)
+
+best_params = study.best_trial.params
+best_model = XGBClassifier(
+    n_estimators=best_params["n_estimators"],
+    max_depth=best_params["max_depth"],
+    learning_rate=best_params["learning_rate"],
+    subsample=best_params["subsample"],
+    colsample_bytree=best_params["colsample_bytree"],
+    min_child_weight=best_params["min_child_weight"],
+    gamma=best_params["gamma"],
+    objective='binary:logistic',
+    use_label_encoder=False,
+    eval_metric="logloss"
+)
+
+best_model.fit(X, y)
+predictions = best_model.predict_proba(droped_test)[:, 1]
+
+submission = pd.DataFrame({'Exited': predictions})
+submission.to_csv("submission.csv", index=False)
